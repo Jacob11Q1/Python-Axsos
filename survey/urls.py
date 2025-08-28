d7c93d0b65d46f67f9c95e43ec247380e3e6e278
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-08-28 20:33:39 +0300

    Delete survey directory

--- survey/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'), # root route to form
-    path('result/', views.result, name='result'), # result route
-]
